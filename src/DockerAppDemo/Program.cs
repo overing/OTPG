408b460695d39981ae67e145b36dfbfa5828d788
CHUN YI, CHANG <chunyi.chang@smartdaily.com.tw>
2025-03-14 14:35:42 +0800

    feat: 移除無關的部分

--- src/DockerAppDemo/Program.cs
+++ src/DockerAppDemo/Program.cs
@@ -1,121 +1,37 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
-using System.Diagnostics.Metrics;
 using Microsoft.AspNetCore.Mvc;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
 using Scalar.AspNetCore;
 
 var appBuilder = WebApplication.CreateBuilder(args);
 
-appBuilder.Logging
-    .ClearProviders()
-    .AddDebug()
-    .AddSimpleConsole();
-
 appBuilder.Services.AddOpenApi();
 
-#region 建立 Meter 與 Counter 加入 OpenTelemetry 與 Prometheus Exporter
-
-appBuilder.Services
-    .AddSingleton<IAppMetrics, AppMetrics>()
-    .AddOpenTelemetry()
-    .WithMetrics(metrics => metrics
-        .ConfigureResource(resource => resource.AddService(serviceName: appBuilder.Environment.ApplicationName))
-        .AddMeter(appBuilder.Environment.ApplicationName)
-        .AddPrometheusExporter());
-
-#endregion
-
-appBuilder.Services
-    .AddSingleton<ConcurrentQueue<string>>();
-
-// appBuilder.Services
-//     .AddHostedService(p => p.GetRequiredService<FakeAccess>())
-//     .AddHttpClient<FakeAccess>(client => client.BaseAddress = new("http://localhost:10254/"));
+appBuilder.Services.AddSingleton<ConcurrentQueue<MyData>>();
 
 var app = appBuilder.Build();
 
 app.MapOpenApi();
 app.MapScalarApiReference();
 
-#region 註冊給 Prometheus 撈取資料的端點並限制 port
+app.MapGet("/", () => Results.Redirect("~/scalar/v1"));
 
-app.MapPrometheusScrapingEndpoint().RequireHost("*:10254");
-
-#endregion
-
-app.MapGet("/action", async ([FromServices] IAppMetrics metrics) =>
-{
-    var sw = Stopwatch.StartNew();
-    var result = Results.Ok("Is work !! :D");
-
-    await Task.Delay(TimeSpan.FromMilliseconds(Random.Shared.Next(100, 300)));
-
-#region 在需要的位置對 Counter 進行操作
-
-    metrics.IncrementRequestCount("/action");
-    metrics.LogRequestHandleTime("/action", sw.ElapsedMilliseconds);
-
-#endregion
-
-    return result;
-});
-
-app.MapPost("/enqueue", ([FromServices] IAppMetrics metrics, [FromServices] ConcurrentQueue<string> queue, [FromBody] ushort count = 256) =>
+app.MapPost("/enqueue", ([FromServices] ConcurrentQueue<MyData> queue, [FromBody] ushort count) =>
 {
     var sw = Stopwatch.StartNew();
 
     for (var i = 0; i < count; i++)
     {
-        queue.Enqueue(Guid.NewGuid().ToString("N"));
+        queue.Enqueue(new() { Id = Guid.NewGuid(), Value = Random.Shared.Next(1000) });
     }
-
-#region 在需要的位置對 Counter 進行操作
-
-    metrics.IncrementRequestCount("/enqueue");
-    metrics.LogRequestHandleTime("/enqueue", sw.ElapsedMilliseconds);
-
-#endregion
 
     return Results.Ok($"enqueue guids x{count} done");
 });
 
 await app.RunAsync();
 
-sealed class AppMetrics : IAppMetrics
+class MyData
 {
-    private readonly Counter<long> _request;
-    private readonly Gauge<long> _elapsedMs;
-
-    public AppMetrics(IHostEnvironment hostEnvironment, IMeterFactory meterFactory)
-    {
-        var meter = meterFactory.Create(name: hostEnvironment.ApplicationName, version: "1.0.0");
-        _request = meter.CreateCounter<long>(name: "request.count", description: "Counts the number of request");
-        _elapsedMs = meter.CreateGauge<long>(name: "request.elapsed_time", unit: "ms");
-    }
-
-    public void IncrementRequestCount(string name) => _request.Add(1, [new("action", name)]);
-
-    public void LogRequestHandleTime(string name, long ms) => _elapsedMs.Record(ms, [new("action", name)]);
-}
-
-interface IAppMetrics
-{
-    void IncrementRequestCount(string name);
-    void LogRequestHandleTime(string name, long ms);
-}
-
-sealed class FakeAccess(HttpClient client) : BackgroundService
-{
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            await Task.Delay(TimeSpan.FromMilliseconds(1000), stoppingToken);
-
-            _ = client.GetAsync("action", stoppingToken);
-        }
-    }
+    public Guid Id { get; set; }
+    public int Value { get; set; }
 }
