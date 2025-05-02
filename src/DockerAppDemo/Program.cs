f81c9e7cda52a3ed42802a9ebe6a9816b3a1dba5
CHUN YI, CHANG <chunyi.chang@smartdaily.com.tw>
2025-05-02 14:59:37 +0800

    feat: 調整展示

--- src/DockerAppDemo/Program.cs
+++ src/DockerAppDemo/Program.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Scalar.AspNetCore;
 
@@ -5,34 +7,42 @@ var appBuilder = WebApplication.CreateBuilder(args);
 
 appBuilder.Services.AddOpenApi();
 
-appBuilder.Services.AddSingleton<IList<Order>, List<Order>>();
+appBuilder.Services.AddSingleton<OrderService>();
 
 var app = appBuilder.Build();
 
 app.MapOpenApi();
 app.MapScalarApiReference(endpointPrefix: "/");
 
-app.MapPost("/api/add_order", ([FromServices] IList<Order> orderCollection, [FromBody] AddOrderParam param) =>
+app.MapPost("/api/order", ([FromServices] OrderService orderService, [FromBody] AddOrderParam param) =>
 {
     if (param is { ProductId.Length: > 0, Count: > 0 })
     {
-        orderCollection.Add(new(Guid.NewGuid(), param.ProductId, param.Count, DateTime.UtcNow));
-        return Results.Ok($"{orderCollection.Count} orders added.");
+        orderService.AddOrder(param);
+        return Results.Ok($"{orderService.Count} orders added.");
     }
 
     return Results.BadRequest();
 });
 
-app.MapPost("/api/dump", () =>
-{
-    var pid = Environment.ProcessId;
-    var filename = $"{DateTime.Now:yyMMdd-HHmmss}.dmp";
-    System.Diagnostics.Process.Start(startInfo: new("dotnet-dump", arguments: $"collect -p {pid} -o {filename}"));
-    return Results.Ok(filename);
-});
-
 await app.RunAsync(url: "http://localhost:5000/");
 
-record class AddOrderParam(string ProductId = "product#1001", ushort Count = 1);
+record class AddOrderParam(
+    [Required(AllowEmptyStrings = false, ErrorMessage = "產品編號是必須的")]
+    string ProductId = "product#1001",
 
+    [Range(minimum: 1, maximum: ushort.MaxValue, ErrorMessage = "數量必須大於 0")]
+    ushort Count = 1
+);
+
+[DebuggerDisplay("#{OrderId}, Product#{ProductId} x{Count} at {OrderTime}")]
 record class Order(Guid OrderId, string ProductId, ushort Count, DateTime OrderTime);
+
+sealed class OrderService
+{
+    private readonly List<Order> _orders = [];
+
+    public int Count => _orders.Count;
+
+    public void AddOrder(AddOrderParam param) => _orders.Add(new(Guid.NewGuid(), param.ProductId, param.Count, DateTime.UtcNow));
+}
